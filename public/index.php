a0696d64887814ca18929b348964c8be38984946
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-06 19:47:13 +0000

    add switch statement for routing

--- public/index.php
+++ public/index.php
@@ -1,9 +1,23 @@
 <?php
 
-require __DIR__ ."/../bootstrap.php";
+require __DIR__ . "/../bootstrap.php";
 
 use SciMuseum\Database;
 
+$request = $_SERVER['REQUEST_URI'];
 $connection = new Database("local", "user", "password");
 
-echo $connection->connectionDetails(); 
+
+switch ($request) {
+    case '/':
+    case '':
+        echo $connection->connectionDetails();
+        echo "<h1>Hello world</h1>";
+        break;
+    case "/admin":
+        echo "<h1>Admin page</h1>";
+        break;
+    default:
+        # code...
+        break;
+}
