4cee40d136a2bb03fb72509e0fabd9596279336f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-18 12:45:05 +0000

    create file for initializing  classes

---
+++ src/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
+
+use Database;
+
+$database = new Database('mysql', '127.0.0.1', 6033, 'ctt_db', 'ctt_user', 'ctt_user_pass');
