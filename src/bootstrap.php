bfa6219f4e411eef573feac75ca6a7247ad5c342
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-18 18:04:08 +0000

    use createTables method

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,3 +5,5 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 use Database;
 
 $database = new Database('mysql', '127.0.0.1', 6033, 'ctt_db', 'ctt_user', 'ctt_user_pass');
+
+$database->createTables('chtt_db.sql');
