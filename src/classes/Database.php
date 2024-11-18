4b073e1c4aed2a26d9e2e02ec1f6f924902cbec2
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-18 12:44:10 +0000

    fix connection string issue

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -1,15 +1,19 @@
 <?php
 
-class Database {
-    protected $connection;
-    public function __construct(PDO $pdo) 
+class Database
+{
+    private $connection;
+    private $query;
+
+    public function __construct(string $dbType, string $host, int $port, string $dbName, string $username, string $password)
     {
         try {
-            $this->connection = $pdo;
+            $this->connection = new PDO("{$dbType}:host={$host};port={$port};dbname={$dbName}", $username, $password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo 'Connection Successful';
         } catch (PDOException $err) {
             echo "Connection Failed: {$err->getMessage()}";
         }
+
     }
 }
