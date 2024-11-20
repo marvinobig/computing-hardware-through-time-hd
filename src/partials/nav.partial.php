931b8d25a62e5fcd3371f5953d1b40363d732ee3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-20 14:26:46 +0000

    correct link

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -2,5 +2,5 @@
     <a href="/login.php">Login</a>
     <a href="/register.php">Register</a>
     <a href="/admin/dashboard.php">Dashboard</a>
-    <a href="/contribution.php">Contributions</a>
+    <a href="/contributions.php">Contributions</a>
 </nav>
