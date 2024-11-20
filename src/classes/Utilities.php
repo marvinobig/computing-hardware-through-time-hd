09bf17d3bf7fd60cc9a14657bdf4763ecba1f95f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-20 16:04:16 +0000

    add partials loader static method

--- src/classes/Utilities.php
+++ src/classes/Utilities.php
@@ -1,5 +1,21 @@
 <?php
 
 class Utilities {
-    // code
+    public static function partial(string $file, string $searchFrom = 'root'):  string|null {
+        try {
+            $sep = DIRECTORY_SEPARATOR;
+
+            switch ($searchFrom) {
+                case "root":
+                    return require_once __DIR__ . $sep . '..' . $sep . 'partials' . $sep . "$file.partial.php";
+                    
+                case "admin":
+                    return require_once __DIR__ . $sep . '..' . $sep . '..' . $sep . 'partials' . $sep . 'nav.partial.php';
+                default:
+                    return "Directory to search from doesn't exist";     
+            }
+        } catch (Exception $err) {
+            return "File Error: {$err->getMessage()}";
+        }
+    }
 }
