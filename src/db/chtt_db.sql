22cc47e1dd3200bb1a102b8632f4cd327898578d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-20 18:29:29 +0000

    add default timestamp

--- src/db/chtt_db.sql
+++ src/db/chtt_db.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `admin_users` (
   `id` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `username` varchar(255) UNIQUE NOT NULL,
   `password_hash` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `hardware` (
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS `hardware` (
   `name` varchar(255) UNIQUE NOT NULL,
   `type` varchar(255) NOT NULL,
   `manufacturer` varchar(255) NOT NULL,
-  `historical_significance_summary` text NOT NULL,
+  `summary` text NOT NULL,
   `details` text NOT NULL,
   `price_at_release` float NOT NULL,
   `status` varchar(255) NOT NULL,
   `user_id` integer NOT NULL,
   `release_date` date NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `hardware_images` (
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `hardware_images` (
   `hardware_id` integer NOT NULL,
   `image_url` text NOT NULL,
   `caption` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `user_activity` (
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `user_activity` (
   `admin_id` integer NOT NULL,
   `action_type` varchar(255) COMMENT 'e.g. Create, Update, Delete' NOT NULL,
   `hardware_id` integer NOT NULL,
-  `happened_at` timestamp NOT NULL
+  `happened_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE `hardware` ADD FOREIGN KEY (`user_id`) REFERENCES `admin_users` (`id`);
