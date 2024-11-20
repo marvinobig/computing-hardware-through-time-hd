28b4dcdc7d267593cbb13c7c35dabd1927d5698d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-20 14:23:04 +0000

    add links

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -1,6 +1,6 @@
 <nav>
-    <a href="">Login</a>
-    <a href="">Register</a>
-    <a href="">Dashboard</a>
-    <a href="">Contributions</a>
+    <a href="/login.php">Login</a>
+    <a href="/register.php">Register</a>
+    <a href="/admin/dashboard.php">Dashboard</a>
+    <a href="/contribution.php">Contributions</a>
 </nav>
