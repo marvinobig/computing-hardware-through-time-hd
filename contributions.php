98be53271f277f64cbd2120fc8802721e98db431
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-20 14:18:46 +0000

    create app pages

--- contributions.php
+++ contributions.php
@@ -1,22 +1,12 @@
 <?php
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "bootstrap.php";
-
-$title = 'Homepage';
-$database->createTables('chtt_db.sql');
-$hardwareListings = $database->query('SELECT * FROM hardware;');
+$title = 'Contributions';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <?php require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'head.partial.php' ?>
 
     <body>
         <?php require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'nav.partial.php' ?>
-
-        <h1>Hello world</h1>
-
-        <?php var_dump($hardwareListings) ?>
     </body>
 
 </html>
---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'Login';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'head.partial.php' ?>
+<body>
+    <?php require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'nav.partial.php' ?>
+</body>
+</html>
