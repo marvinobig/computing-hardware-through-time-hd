c47a75d20446f685472dc7d5bb5d60a7332e227a
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-21 15:15:01 +0000

    change db credentials

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,4 +5,4 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 use Database;
 use Utilities;
 
-$database = new Database('mysql', '127.0.0.1', 6033, 'ctt_db', 'ctt_user', 'ctt_user_pass');
+$database = new Database('mysql', '127.0.0.1', 6033, 'chtt_db', 'chtt_user', 'chtt_user_pass');
