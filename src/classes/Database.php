522da82a0fa171a155e2aef20d981f13e2b93738
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-21 15:03:30 +0000

    alter connection statement

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -8,7 +8,7 @@ class Database
     public function __construct(string $dbType, string $host, int $port, string $dbName, string $username, string $password)
     {
         try {
-            $this->connection = new PDO("{$dbType}:host={$host};port={$port};dbname={$dbName}", $username, $password);
+            $this->connection = new PDO("{$dbType}:host={$host};port={$port};dbname={$dbName};charset=utf8", $username, $password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $err) {
