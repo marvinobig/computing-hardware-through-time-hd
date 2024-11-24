a239635fc2eaea27ae90c6b31d2d0104e693a04c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-24 15:31:26 +0000

    add default heading if no listings are available

--- contributions.php
+++ contributions.php
@@ -19,21 +19,27 @@ $hardwareListings = $database->query('SELECT * FROM hardware;');
         <main>
             <h1>Contributions</h1>
 
-            <?php foreach ($hardwareListings as $hardware): ?>
-                <a href="/contribution.php?id=<?= $hardware['id'] ?>">
-                    <article>
-                        <img src="<?= $hardware['image_url'] ?>" alt="<?= $hardware['name'] ?>">
-                        <div>
-                            <h3><?= $hardware['name'] ?></h3>
-                            <p><?= nl2br($hardware['summary'])  ?></p>
-                            <div>
-                                <p><?= $hardware['status'] ?></p>
-                                <p>Release Date: <?= nl2br($hardware['release_date']) ?></p>
-                            </div>
-                        </div>
-                    </article>
-                </a>
-            <?php endforeach; ?>
+            <section>
+                <?php if ($hardwareListings): ?>
+                    <?php foreach ($hardwareListings as $hardware): ?>
+                        <a href="/contribution.php?id=<?= $hardware['id'] ?>">
+                            <article>
+                                <img src="<?= $hardware['image_url'] ?>" alt="<?= $hardware['name'] ?>">
+                                <div>
+                                    <h3><?= $hardware['name'] ?></h3>
+                                    <p><?= nl2br($hardware['summary']) ?></p>
+                                    <div>
+                                        <p><?= $hardware['status'] ?></p>
+                                        <p>Release Date: <?= nl2br($hardware['release_date']) ?></p>
+                                    </div>
+                                </div>
+                            </article>
+                        </a>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <h2>Nothing to Show</h2>
+                <?php endif ?>
+            </section>
         </main>
     </body>
 
