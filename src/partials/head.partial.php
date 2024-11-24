c82de684399fcd92d3e65ca7015040673c42885f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-24 15:25:34 +0000

    use proper path to js file

--- src/partials/head.partial.php
+++ src/partials/head.partial.php
@@ -2,5 +2,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHTT: <?= $title ?></title>
-    <script type="module" src="index.js" defer></script>
+    <script type="module" src="/assets/index.js" defer></script>
 </head>
