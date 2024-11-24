056ccace2f72c2b8531ad1ef5b1de3363bdc88d8
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-24 16:45:22 +0000

    utilize ajax to add listings

--- api/create-listing.php
+++ api/create-listing.php
@@ -1,13 +1,17 @@
 <?php
 
-if (isset($_FILES['image'])) {
+require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "bootstrap.php";
+
+use App\Utilities;
+
+if (isset($_FILES['image']) && isset($_POST)) {
     if ($_FILES['image']['error'] == 0) {
         $imageFileName = uniqid() . '_' . str_replace(' ', '_', $_FILES['image']['name']);
         $imageFileTmpName = $_FILES['image']['tmp_name'];
         $imageFileSize = $_FILES['image']['size'];
         $imageFileType = $_FILES['image']['type'];
 
-        $imageTargetDir = __DIR__ . DIRECTORY_SEPARATOR . "hardware_image_uploads" . DIRECTORY_SEPARATOR;
+        $imageTargetDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR. "hardware_image_uploads" . DIRECTORY_SEPARATOR;
 
         if (!is_dir($imageTargetDir)) {
             mkdir($imageTargetDir, 0777, true);
@@ -16,17 +20,15 @@ if (isset($_FILES['image'])) {
         $allowedImageTypes = ['image/jpeg', 'image/png', 'image/gif'];
 
         if (!in_array($imageFileType, $allowedImageTypes)) {
-            echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
+            Utilities::sendJson(400, [
+                'msg' => 'Sorry, only JPG, JPEG, PNG, and GIF files are allowed for the hardware image'
+            ]);
         } else if ($imageFileSize > 5000000) {
-            echo "Sorry, your file is too large.";
+            Utilities::sendJson(400, [
+                'msg' => 'The hardware image file is too large'
+            ]);
         } else {
-            if (move_uploaded_file($imageFileTmpName, $imageTargetDir . basename($imageFileName))) {
-                echo "The file " . basename($imageFileName) . " has been uploaded successfully.";
-            } else {
-                echo "Sorry, there was an error uploading your main image.";
-            }
-
-            $database->query('INSERT INTO hardware (image_url, name, type, manufacturer, summary, details, price_at_release, status, user_id, release_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+            $dbResponse = $database->query('INSERT INTO hardware (image_url, name, type, manufacturer, summary, details, price_at_release, status, user_id, release_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
                 str_replace(__DIR__, '', $imageTargetDir) . $imageFileName,
                 $_POST['hardware_name'],
                 $_POST['hardware_type'],
@@ -37,9 +39,27 @@ if (isset($_FILES['image'])) {
                 $_POST['status'],
                 1,
                 $_POST['release_date']
-            ]);
+            ], 'json');
+
+            if ($dbResponse) {
+                if (move_uploaded_file($imageFileTmpName, $imageTargetDir . basename($imageFileName))) {
+                    Utilities::sendJson(200, [
+                        'msg' => 'The hardware and its image has successfully been listed'
+                    ]);
+                }
+            } else {
+                Utilities::sendJson(400, [
+                    'msg' => 'The hardware and its image has failed to be listed',
+                    'data' => [
+                        $_FILES['image'],
+                        $_POST,
+                    ]
+                ]);
+            }
         }
     }
 } else {
-    echo "No file was uploaded or there was an error.";
+    Utilities::sendJson(400, [
+        'msg' => 'There is an error with the form used to send the data'
+    ]);
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+console.log("hello world");
+
+const contributionsInsertForm = document.querySelector("#contributions_upload");
+
+contributionsInsertForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch("/api/create-listing.php", {
+      method: "post",
+      body: formData,
+    });
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`Form data was not uploaded successfully: ${result.msg}`);
+    } else {
+        alert(result.msg);
+        form.reset();
+    }
+  } catch (err) {
+    alert(err.message);
+  }
+});
