21bdc7b9f095de7b380de6361945a5e5af9bc83b
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-25 15:58:57 +0000

    link css file

--- src/partials/head.partial.php
+++ src/partials/head.partial.php
@@ -3,4 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHTT: <?= $title ?></title>
     <script type="module" src="/assets/js/index.js" defer></script>
+    <link rel="stylesheet" href="/assets/main.css">
 </head>
