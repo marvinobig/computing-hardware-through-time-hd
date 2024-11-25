1658451825bd3880bf7fe4106c58255836737f4e
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-25 19:19:00 +0000

    add ajax powered delete

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,43 @@
 console.log("hello world");
 
 const contributionsInsertForm = document.querySelector("#contributions_upload");
-const contributionsTable = document.querySelector('#listings_table');
+const contributionsTable = document.querySelector("#contributions_table");
+const contributionsTableBody = document.querySelector(
+  "#contributions_table tbody"
+);
+
+async function reloadContributionsTable(tableBody) {
+  try {
+    const response = await fetch("/api/load-listings.php");
+    const result = await response.json();
+
+    tableBody.innerHTML = "";
+
+    result.listings.forEach((listing) => {
+      const createdDate = new Date(listing.created_at);
+
+      const row = `
+        <tr>
+          <td>
+            <a href="/contribution.php?id=${listing.id}">
+              ${listing.name}
+            </a>
+          </td>
+          <td>${listing.views}</td>
+          <td>${createdDate.toLocaleDateString()} at ${createdDate.toLocaleTimeString()}</td>
+          <td>
+            <button id="${listing.id}" type="button">Edit</button>
+            <button id="${listing.id}" type="button">Delete</button>
+          </td>
+        </tr>
+      `;
+
+      tableBody.insertAdjacentHTML("beforeend", row);
+    });
+  } catch (err) {
+    console.error(`Error loading contributions: ${err}`);
+  }
+}
 
 if (contributionsInsertForm) {
   contributionsInsertForm.addEventListener("submit", async (event) => {
@@ -18,12 +54,11 @@ if (contributionsInsertForm) {
       const result = await response.json();
 
       if (!response.ok) {
-        throw new Error(
-          `Form data was not uploaded successfully: ${result.msg}`
-        );
+        throw new Error(`Form data was not uploaded successfully`);
       } else {
         alert(result.msg);
         form.reset();
+        reloadContributionsTable(contributionsTableBody);
       }
     } catch (err) {
       alert(err.message);
@@ -31,3 +66,32 @@ if (contributionsInsertForm) {
   });
 }
 
+if (contributionsTable) {
+  contributionsTable.addEventListener("click", async (event) => {
+    if (event.target.type === "button") {
+      const confirmation = prompt(
+        "Type 'delete' to confirm deletion of listing"
+      );
+
+      if (confirmation === "delete") {
+        const listingId = event.target.id;
+        const responseMsg = "Listing has been deleted";
+
+        try {
+          const response = await fetch(
+            `/api/delete-listing.php?id=${listingId}`
+          );
+
+          if (!response.ok) {
+            throw new Error(responseMsg);
+          } else {
+            alert(responseMsg);
+            reloadContributionsTable(contributionsTableBody);
+          }
+        } catch (err) {
+          alert(err.message);
+        }
+      }
+    }
+  });
+}
