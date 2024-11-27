899ff825318d1fa253cfda180f6ed0c2631ccab6
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-11-27 11:45:03 +0000

    change to implement password protection strategy

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -3,9 +3,8 @@
     <section>
         <a href="/contributions.php">Contributions</a>
         <div>
-            <?php if (!isset($_SESSION['user'])): ?>
-                <a href="/login.php">Login</a>
-                <a href="/register.php">Register</a>
+            <?php if (!isset($_SESSION['admin_user'])): ?>
+                <a href="/admin-login.php">Login</a>
             <?php else: ?>
                 <a href="/admin/dashboard.php">Dashboard</a>
             <?php endif; ?>
