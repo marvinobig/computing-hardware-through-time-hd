6890044d797f8c3b52603701310714aaa9b0172a
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-12 15:23:49 +0000

    change session key

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -3,7 +3,7 @@
     <section>
         <a href="/contributions.php">Contributions</a>
         <div>
-            <?php if (!isset($_SESSION['admin_user'])): ?>
+            <?php if (!isset($_SESSION['user'])): ?>
                 <a href="/admin/auth/login.php">Login</a>
             <?php else: ?>
                 <a href="/admin/dashboard.php">Dashboard</a>
