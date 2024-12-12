1a8a74b60a04ea62a5cd68752fcdfb97d7092235
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-12 15:29:51 +0000

    start session from bootstrap file

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 
+session_set_cookie_params(1800);
+session_start();
+
 use Data\Database;
 
 $database = new Database('mysql', '127.0.0.1', 6033, 'chtt_db', 'chtt_user', 'chtt_user_pass');
