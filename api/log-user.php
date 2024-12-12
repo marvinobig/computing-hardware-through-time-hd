7edaa5db3e8f2c2f2b4d4098cc36a9e340834dfc
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-12 15:36:36 +0000

    remove call to session functions

--- api/log-user.php
+++ api/log-user.php
@@ -4,9 +4,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "src" 
 use App\Utilities;
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-    session_set_cookie_params(1800);
-    session_start();
-
     $username = $_POST["username"];
     $password = $_POST["password"];
 
