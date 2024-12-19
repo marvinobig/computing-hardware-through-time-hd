87ec5d14c112f2227d4c7a52e862d02ebcfe84f3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-19 16:35:21 +0000

    style nav bar

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -1,12 +1,14 @@
 <nav>
-    <a id="site-title" href="/">CHTT</a>
-    <section>
-        <a href="/contributions.php">Contributions</a>
-        <?php if (!isset($_SESSION['user'])): ?>
-            <a href="/admin/auth/login.php">Login</a>
-        <?php else: ?>
-            <a href="/admin/dashboard.php">Dashboard</a>
-            <a id="logout" href="/api/logout.php">Logout</a>
-        <?php endif; ?>
-    </section>
+    <div id="inner-nav">
+        <a id="site-title" href="/">CHTT</a>
+        <section>
+            <a href="/contributions.php">Contributions</a>
+            <?php if (!isset($_SESSION['user'])): ?>
+                <a href="/admin/auth/login.php">Login</a>
+            <?php else: ?>
+                <a href="/admin/dashboard.php">Dashboard</a>
+                <a id="logout" href="/api/logout.php">Logout</a>
+            <?php endif; ?>
+        </section>
+    </div>
 </nav>
