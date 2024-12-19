1836a4b721ffb2e91bc9c1661fe68df43f9b0630
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-19 16:04:37 +0000

    remove unnecessary div

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -2,13 +2,11 @@
     <a href="/">CHTT</a>
     <section>
         <a href="/contributions.php">Contributions</a>
-        <div>
-            <?php if (!isset($_SESSION['user'])): ?>
-                <a href="/admin/auth/login.php">Login</a>
-            <?php else: ?>
-                <a href="/admin/dashboard.php">Dashboard</a>
-                <a href="/api/logout.php">Logout</a>
-            <?php endif; ?>
-        </div>
+        <?php if (!isset($_SESSION['user'])): ?>
+            <a href="/admin/auth/login.php">Login</a>
+        <?php else: ?>
+            <a href="/admin/dashboard.php">Dashboard</a>
+            <a href="/api/logout.php">Logout</a>
+        <?php endif; ?>
     </section>
 </nav>
