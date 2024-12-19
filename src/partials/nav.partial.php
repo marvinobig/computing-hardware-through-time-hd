67f34a38c5ee239056b15142b5b3e99c73e60d24
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-12-19 16:21:16 +0000

    add id to logout button

--- src/partials/nav.partial.php
+++ src/partials/nav.partial.php
@@ -6,7 +6,7 @@
             <a href="/admin/auth/login.php">Login</a>
         <?php else: ?>
             <a href="/admin/dashboard.php">Dashboard</a>
-            <a href="/api/logout.php">Logout</a>
+            <a id="logout" href="/api/logout.php">Logout</a>
         <?php endif; ?>
     </section>
 </nav>
