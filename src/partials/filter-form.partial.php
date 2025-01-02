38b535183fbbd783855eab6c60b861261b6f646c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-01-02 15:05:13 +0000

    style filter form

--- src/partials/filter-form.partial.php
+++ src/partials/filter-form.partial.php
@@ -15,47 +15,44 @@ $statuses = $database->query("SELECT DISTINCT status as status FROM hardware");
     Filters
 </button>
 <dialog id="filters_form">
-    <section>
+    <section class="form_heading">
         <h2>Filter Contributions</h2>
-        <button id="filters_form_close_btn" type="button">Close</button>
+        <button id="filters_form_close_btn" type="button" class="btn">Close</button>
     </section>
     <form method="get">
-        <section>
-            <label for="year-from">Year (From)</label>
+        <label for="year-from">Year (From)
             <select name="year-from" id="year-from">
                 <option selected value="none">N/A</option>
                 <?php foreach ($yearsAsc as $year): ?>
                     <option value="<?= $year['year'] ?>"><?= $year['year'] ?></option>
                 <?php endforeach; ?>
             </select>
-        </section>
-        <section>
-            <label for="year-to">Year (To)</label>
+        </label>
+        <label for="year-to">Year (To)
             <select name="year-to" id="year-to">
                 <option selected value="none">N/A</option>
                 <?php foreach ($yearsDesc as $year): ?>
                     <option value="<?= $year['year'] ?>"><?= $year['year'] ?></option>
                 <?php endforeach; ?>
             </select>
-        </section>
-        <section>
-            <label for="type">Hardware Type</label>
+        </label>
+        <label for="type">Type
             <select name="type" id="type">
                 <option selected value="none">N/A</option>
                 <?php foreach ($types as $type): ?>
                     <option value="<?= $type['type'] ?>"><?= strtoupper($type['type']) ?></option>
                 <?php endforeach; ?>
             </select>
-        </section>
-        <section>
-            <label for="status">Status</label>
+        </label>
+        <label for="status">Status
             <select required name="status" id="status">
                 <option selected value="none">N/A</option>
                 <?php foreach ($statuses as $status): ?>
-                    <option value="<?= $status['status'] ?>"><?= ucwords(str_replace('_', ' ', $status['status'])) ?></option>
+                    <option value="<?= $status['status'] ?>"><?= ucwords(str_replace('_', ' ', $status['status'])) ?>
+                    </option>
                 <?php endforeach; ?>
             </select>
-        </section>
-        <button type="submit">Filter Contributions</button>
+        </label>
+        <button type="submit" class="btn">Filter Contributions</button>
     </form>
 </dialog>
