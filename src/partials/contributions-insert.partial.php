508a245fb3afeb7b236973e8c4582fa78cb1c349
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-01-03 17:35:47 +0000

    style contribution insert form

--- src/partials/contributions-insert.partial.php
+++ src/partials/contributions-insert.partial.php
@@ -1,19 +1,18 @@
 <dialog id="create_modal">
-    <div>
-        <h2>Contribution</h2>
+    <div class="header">
+        <h2 class="remove-mg">Contribution</h2>
         <button id="create_modal_close_btn" type="button" class="btn">Close</button>
     </div>
     <form id="contributions_upload" method="post" enctype="multipart/form-data">
-        <section>
-            <label for="image">Image</label>
+        <label for="image">Image
             <input required type="file" name="image" id="image" accept="image/*">
-        </section>
-        <section>
-            <label for="hardware_name">Name</label>
+        </label>
+
+        <label for="hardware_name">Name
             <input required type="text" name="hardware_name" id="hardware_name">
-        </section>
-        <section>
-            <label for="hardware_type">Type</label>
+        </label>
+
+        <label for="hardware_type">Type
             <select required name="hardware_type" id="hardware_type">
                 <option value="cpu">CPU (Processor)</option>
                 <option value="gpu">GPU (Graphics Processing Unit)</option>
@@ -28,40 +27,34 @@
                 <option value="case">PC Case</option>
                 <option value="peripherals">Peripherals (Mouse, Keyboard, etc.)</option>
             </select>
-        </section>
-        <section>
-            <label for="manufacturer">Manufacturer</label>
+        </label>
+
+        <label for="manufacturer">Manufacturer
             <input required type="text" name="manufacturer" id="manufacturer">
-        </section>
-        <section>
-            <label for="summary">Historical Significance Summary</label>
-            <textarea name="summary" id="summary" rows="4" cols="50"></textarea>
-        </section>
-        <section>
-            <label for="details">Details</label>
-            <textarea name="details" id="details" rows="10" cols="50"></textarea>
-        </section>
-        <section>
-            <label for="price_at_release">Price at Release</label>
+        </label>
+
+        <label for="summary">Historical Significance Summary</label>
+        <textarea name="summary" id="summary" rows="4" cols="50"></textarea>
+        <label for="details">Details</label>
+        <textarea name="details" id="details" rows="10" cols="50"></textarea>
+        <label for="price_at_release">Price at Release
             <input required type="number" step="0.01" min="0.00" name="price_at_release" id="price_at_release">
-        </section>
-        <section>
-            <label for="status">Status</label>
+        </label>
+
+        <label for="status">Status
             <select required name="status" id="status">
                 <option value="na">N/A</option>
                 <option value="fully_supported">Fully Supported</option>
                 <option value="limited_support">Limited Support</option>
                 <option value="obsolete">Obsolete</option>
             </select>
-        </section>
-        <section>
-            <label for="release_date">Release Date</label>
-            <input required type="date" name="release_date" id="release_date">
-        </section>
+        </label>
 
-        <section>
-            <button type="submit" class="btn">Add to Contributions</button>
-        </section>
+        <label for="release_date">Release Date
+            <input required type="date" name="release_date" id="release_date">
+        </label>
+
+        <button type="submit" class="btn">Add to Contributions</button>
     </form>
 </dialog>
 <button id="create_modal_open_btn" type="button" class="btn">
