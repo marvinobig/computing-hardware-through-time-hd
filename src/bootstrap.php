e984edaddef9b97a61e3cfebd09c87dd37f144d5
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-20 17:21:56 +0200

    turn off warnings

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,9 +2,10 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 session_set_cookie_params(1800);
 session_start();
 
 use Data\Database;
 
-$database = new Database('mysql', '127.0.0.1', 6033, 'chtt_db', 'chtt_user', 'chtt_user_pass');
+$database = new Database('mysql', 'chtt_db', 3306, 'chtt_db', 'chtt_user', 'chtt_user_pass');
